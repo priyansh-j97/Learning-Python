dc80b1502caf5cc2e9939ecf63c28a244e5ba1f7
Priyansh Jain <34898188+priyansh-j97@users.noreply.github.com>
2021-06-02 04:01:21 +0530

    First Commit

---
+++ numbers.py
@@ -0,0 +1,12 @@
+print("Addition: ", 5+41) #addition
+print("Subtraction: ", 1-9) #subtraction
+print("Multiplication: ", 5*4) #multiplication
+print("Division: ", 10/3) #division
+print("\n")
+print("Round-off +ve number: ", 10//3) #"//" automatically rounds off to the neearest whole number which is less than the actual number
+print("Round-off -ve number: ", -10//4)
+print("\n")
+print("Modulus: ", 18%4) #modulus
+print("\n")
+print("Multiplication: ", 5*5*5)
+print("Power: ", 5**3) #"**" means power
---
+++ variables.py
@@ -0,0 +1,3 @@
+hello = 1
+world = .5 #python auto assignes datatypes to variables as per the value stored in it
+print("Hello World: ", hello*world)

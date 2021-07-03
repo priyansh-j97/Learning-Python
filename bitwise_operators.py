e6ba8eda2ea5bcc59ce5feacaa9f86cb0320abb9
Priyansh Jain <34898188+priyansh-j97@users.noreply.github.com>
2021-07-04 03:19:58 +0530

    Fourteenth Commit

---
+++ bitwise_operators.py
@@ -0,0 +1,14 @@
+a=50 #110010
+b=25  #011001
+
+# binary "AND" 
+c=a & b #this will give out "010000", which is "16"
+print(c)
+
+# binary "LEFT SHIFT"
+d=a << 2 #this will give out "11001000", which is "200"
+print(d)
+
+# binary "RIGHT SHIFT"
+e=a >> 2 #this will give out "001100", which is "12"
+print(e)

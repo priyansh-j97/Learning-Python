f1e1e5879de1612f8aae4a298a4d6344c53c0209
Priyansh Jain <34898188+priyansh-j97@users.noreply.github.com>
2021-07-04 03:20:08 +0530

    Fourteenth Commit

---
+++ heapq_python.py
@@ -0,0 +1,16 @@
+import heapq
+
+# for the case of a simple "list"
+nums=[5,4,8,44,845,669]
+print(heapq.nlargest(3,nums)) #this will print the largest 3 numbers of the "list"
+print(heapq.nsmallest(2,nums))
+
+# for the case of a "dictionary"
+stud=[
+    {'name':'Priyansh','roll':'55'},
+    {'name':'Alex','roll':'68'},
+    {'name':'Felix','roll':'98'},
+    {'name':'Naman','roll':'87'}
+]
+print(heapq.nsmallest(2,stud,key=lambda x: x['roll']))
+print(heapq.nlargest(2,stud,key=lambda x: x['name']))
